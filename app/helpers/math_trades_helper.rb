91defc3a789fc3eeb34085a3d5482a357ed738f8
Jake Roussel <jakeroussel@mac.com>
2016-05-07 22:49:34 -0500

    resync

--- app/helpers/math_trades_helper.rb
+++
@@ -1,2 +0,0 @@
-module MathTradesHelper
-end
--- db/migrate/20160506203902_add_bgg_account_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddBggAccountToUsers < ActiveRecord::Migration
-  def change
-	  add_column :users, :bgg_account, :string, after: :last_sign_in_ip
-	  add_column :users, :bgg_account_verification_code, :string, after: :bgg_account
-	  add_column :users, :bgg_account_verified, :boolean, after: :bgg_account_verification_code, null: false, default: false
-  end
-end
