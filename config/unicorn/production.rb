e84a9bdb65a35b68a0cead1e968d34bd5f510fb0
Jake Roussel <jakeroussel@mac.com>
2016-05-15 11:57:16 -0500

    more config update

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -10,7 +10,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "/var/run/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
