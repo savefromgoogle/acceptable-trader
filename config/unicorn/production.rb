28bd358afdecf06b5f9a47ab30525a63f4fd2997
Jake Roussel <jakeroussel@mac.com>
2016-05-15 12:27:00 -0500

    more production data

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,5 @@
 # set path to application
 app_dir = "/home/rails/acceptable-trader"
-shared_dir = "#{app_dir}/tmp"
 working_directory app_dir
 
 
@@ -17,4 +16,29 @@ stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
