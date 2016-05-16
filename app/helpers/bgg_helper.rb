bc7fdf75e0f965af15d7ccd0987b34800ee45204
Jake Roussel <jakeroussel@mac.com>
2016-05-15 21:51:08 -0500

    Not entirely sure what's going on here.

--- app/helpers/bgg_helper.rb
+++ app/helpers/bgg_helper.rb
@@ -47,8 +47,8 @@ module BggHelper
 	def self.get_item(id)
 		value = Rails.cache.fetch("bgg_item_#{id}/data");
 		if value.nil?
-			value = Rails.cache.fetch("bgg_item_#{id}/data", expires_in: 7.days) do
-				new_item = BoardGameGem.get_item(id, true) rescue value
+			value = Rails.cache.fetch("bgg_item_#{id}/data", expires_in: 14.days) do
+				new_item = BoardGameGem.get_item(id, true)
 				if new_item.nil?
 					value
 				else
