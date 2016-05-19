cf3228f5ce90741062ca258d86dffa2de034d1d7
Jake Roussel <jakeroussel@mac.com>
2016-05-18 20:12:12 -0500

    Implemented user importer service

--- app/services/bgg_importer_service.rb
+++ app/services/bgg_importer_service.rb
@@ -1,6 +1,5 @@
 module BggImporterService
 	@@items_queue = []
-	
 	def self.enqueue_items(ids)
 		@@items_queue += (ids - @@items_queue)
 	end
@@ -15,7 +14,9 @@ module BggImporterService
 			 .map{ |x| x["bgg_item_id"].to_i }
 			self.enqueue_items(ids)
 			
-			Rails.logger.debug("#{@@items_queue.length} game data items in queue.")
+			missing_users = User.where(bgg_user_data_id: nil)
+			
+			Rails.logger.debug("#{@@items_queue.length} game records, #{missing_users.length} user records unimported.")
 			if @@items_queue.length > 0
 				items_to_fetch = @@items_queue.shift(25)
 				items = BggHelper.fetch_items(items_to_fetch)
@@ -24,6 +25,12 @@ module BggImporterService
 				if items_missing.length > 0
 					enqueue_items(items_missing)
 				end
+			end
+			
+			missing_users.each do |missing_user|
+				bgg_user = BggHelper.get_user(missing_user.bgg_account)
+				missing_user.bgg_user_data_id = bgg_user.id
+				missing_user.save
 			end
 		end
 	end
